177dfa30049ccd8e98f5a313f0e4cb71b4329bee
sulianggang <sulianggang@askingdata.com>
2016-12-13 21:52:12 +0800

    模糊匹配指令

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -9,6 +9,7 @@ angular.module('searchModel',[])
 	        {name: '被单', id: '06'},
 	        {name: '被套', id: '07'}
 	    ];
+
 	})
 
 
@@ -17,286 +18,141 @@ angular.module('searchModel',[])
 
 
 
+/**
+ * 搜索自动提示
+ * @parame placeholder: input提示内容
+ * @parame url: 请求url地址
+ * @parame searchContent: 绑定控制器中ng-model的参数
+ * @parame firstParameter: 返回结果对象数组是否有上一级，有的话就填这个
+ * @parame searchParameter: 在数据库中需要匹配的参数
+ */
 
+ .directive('autoMarch',function ($http, $timeout) {
+     return {
+         restrict :'E',
+         scope: {
+             "placeholder": "@",
+             'url': '@',
+             'searchContent': '=',
+             'firstParameter': '@',
+             'searchParameter': '@'
+         },
+         template :`<div>
+                         <input type="text" placeholder="{{placeholder}}" ng-model="searchContent" ng-focus="showResult()" ng-blur="hideResult()" />
+                         <div class="auto-dropdown" ng-if="dropDown" ng-mouseleave="hoverLeave()">
+                             <div class="auto-result">
+                                 <ul>
+                                     <li ng-repeat="result in results track by $index" ng-bind-html="result" ng-mouseover="hoverRow($index)" ng-click="selectRow($index)" ng-class="{'auto-select-row': $index == selectIndex}"></li>
+                                 </ul>
+                             </div>
+                         </div>
+                    </div>`,
+         link: function ($scope, elem, attrs) {
+             $scope.selectIndex = -1;
+             $scope.results = [];
+             //绑定表单input键入事件
+             elem.find('input').on('keyup', function(event){   
+                 if (!(event.which == 38 || event.which == 40 || event.which == 13)) {      //keyCode 38:向上箭头 40向下箭头 13回车键
+                     $timeout.cancel($scope.handleDelay);                              //每次键入都先cancel掉前面的定时器，保证页面中没有过多的定时器
+                     $scope.handleDelay = $timeout($scope.handleEmpty(),50)           //延迟500毫秒，保证每次改变值都能改变，如果不加上的话会延迟一位字符             
+                 } else {
+                     if($scope.results.length > 0){
+                           switch(event.which) {
+                               case 40 :
+                                 if($scope.selectIndex >= $scope.results.length-1) {
+                                     $scope.selectIndex = -1;
+                                 }
+                                 $scope.selectIndex ++;
+                                 break;
+                               case 38 :
+                                 if($scope.selectIndex <= 0) {
+                                     $scope.selectIndex = $scope.results.length;
+                                 }
+                                 $scope.selectIndex --;
+                                 break;
+                               case 13 :
+                                 $scope.selectRow($scope.selectIndex);
+                                 $scope.$apply();
+                                 break;
 
-.directive('autoMarch',function () {
-    return {
-        restrict :'E',
-        scope: {
-            "placeholder": "@"
-        },
-        template :`<div>
-                        <input type="text" placeholder="{{placeholder}}" ng-model="searchContent" />
-                        <div ng-if="dropDown">
-                            <div ng-show="searching">搜索中···</div>
-                        </div>
-                   </div>`,
-        link: function (scope, elem, attrs) {
-            elem.find('input').on('keyup', function(event){             
-                if (!(event.which == 38 || event.which == 40 || event.which == 13)) {      //keyCode 38:向上箭头 40向下箭头 13回车键
-                    if (scope.searchContent == '') {                                //如果搜索无内容，则隐藏所有显示
-                        console.log('空')
-                    }
-                    console.log('不空')
-                } else {
-                    event.preventDefault();                              //如果是有上面几个键，则阻止事件发生，即,排除掉这几个按键
-                }
-            })
+                           } 
+                           $scope.$apply();              //$scope.$apply()是用来检测selectIndex数据变化（angular库之外的才会用到）
+                           event.preventDefault();       //取消与事件关联的默认动作
+                           event.stopPropagation();      //停止事件传播
+                     }
+                 }
+             })
 
-            
-        }
-    }
-})
+             //处理空值
+             $scope.handleEmpty = function () {   
+                 $scope.results = [];   
+                 $scope.selectIndex = -1;               
+                 if ($scope.searchContent == '') {                                //如果搜索无内容，则隐藏所有显示
+                     $scope.dropDown = false;
+                 }else  {
+                     $scope.handleSearchLogic($scope.searchContent);     //开始处理搜索                    
+                 }             
+             }
 
+             //处理搜索逻辑 searchContent： 输入的搜索内容
+             $scope.handleSearchLogic = function (searchContent) {
+                 $scope.num = $scope.searchContent.length;
+                 $http.get($scope.url + searchContent).success(function (data) {
+                     $scope.handleSearchResult((($scope.firstParameter) ? data[$scope.firstParameter] : data ), searchContent);  //三元表达式的目的是当返回的结果目标数组对象如果有上一级key的话就处理，没有的话就直接输出
+                 })
+             }
 
+             //处理搜索结果 respondContent: 请求模糊查询返回的数据库的数组对象   searchContetn: 输入的搜索内容
+             $scope.handleSearchResult = function (respondContent, searchContent) {
+                 if (respondContent && respondContent.length > 0) {           //返回结果不为空
+                     $scope.dropDown = true; 
+                     $scope.beforeResults = [];   //用来存放未处理过的数组，在点击显示的时候有用
+                     $scope.results = [];         //定义一个数组来存放所有结果
+                     for (var i = 0; i< respondContent.length; i++) {
+                         var resultsStr = respondContent[i][$scope.searchParameter];
+                         var reg = new RegExp(searchContent, 'i')       //为搜索内容创建一个正则对象，忽略大小写
+                         var regMatch = resultsStr.match(reg)[0]                    //.match()方法规定要匹配的模式为 RegExp 对象，输出的是忽略大小的匹配结果
+                         var regComplete = resultsStr.replace(reg, '<span class="auto-hightlight">'+ regMatch +'</span>')         //.replace()将正则对象替换掉
+                         $scope.results.push(regComplete);
+                         $scope.beforeResults.push(resultsStr)
 
+                     }
+                 } else {
+                     $scope.dropDown = false;
+                      $scope.results = []; 
+                 }
+             }  
 
+             //鼠标移到相应的位置得到index，来区分每条记录 
+             $scope.hoverRow = function (index) {
+                 $scope.selectIndex = index;
+             }
 
+             //点击赋值
+             $scope.selectRow = function (index) {
+                 $scope.searchContent = $scope.beforeResults[index];
+                 $scope.dropDown = false;
+             }
 
+             //显示结果(当有结果的时候菜显示)
+             $scope.showResult = function () {
+                 if($scope.results.length > 0){              
+                     $scope.dropDown = true;
+                 }
+             }
 
+             //隐藏结果(没有选中的时候才允许隐藏)
+             $scope.hideResult = function () {               
+                 if($scope.selectIndex < 0) {               
+                     $scope.dropDown = false;
+                 }              
+             }
 
-
-
-
-
-
-    .directive('angucomplete', function ($parse, $http, $sce, $timeout) {
-    return {
-        restrict: 'EA',
-        scope: {
-            "id": "@id",
-            "placeholder": "@placeholder", //X
-            "selectedObject": "=selectedobject",
-            "url": "@url",
-            "dataField": "@datafield",
-            "titleField": "@titlefield",
-            "descriptionField": "@descriptionfield",
-            "imageField": "@imagefield",
-            "imageUri": "@imageuri",
-            "inputClass": "@inputclass",
-            "userPause": "@pause",
-            "localData": "=localdata",
-            "searchFields": "@searchfields",
-            "minLengthUser": "@minlength",
-            "matchClass": "@matchclass"
-        },
-        template: '<div class="angucomplete-holder"><input id="{{id}}_value" ng-model="searchStr" type="text" placeholder="{{placeholder}}" class="{{inputClass}}" onmouseup="this.select();" ng-focus="resetHideResults()" ng-blur="hideResults()" /><div id="{{id}}_dropdown" class="angucomplete-dropdown" ng-if="showDropdown"><div class="angucomplete-searching" ng-show="searching">搜索中...</div><div class="angucomplete-searching" ng-show="!searching && (!results || results.length == 0)">无返回结果</div><div class="angucomplete-row" ng-repeat="result in results" ng-mousedown="selectResult(result)" ng-mouseover="hoverRow()" ng-class="{\'angucomplete-selected-row\': $index == currentIndex}"><div ng-if="imageField" class="angucomplete-image-holder"><img ng-if="result.image && result.image != \'\'" ng-src="{{result.image}}" class="angucomplete-image"/><div ng-if="!result.image && result.image != \'\'" class="angucomplete-image-default"></div></div><div class="angucomplete-title" ng-if="matchClass" ng-bind-html="result.title"></div><div class="angucomplete-title" ng-if="!matchClass">{{ result.title }}</div><div ng-if="result.description && result.description != \'\'" class="angucomplete-description">{{result.description}}</div></div></div></div>',
-
-        link: function($scope, elem, attrs) {
-            $scope.lastSearchTerm = null;
-            $scope.currentIndex = null;
-            $scope.justChanged = false;
-            $scope.searchTimer = null;
-            $scope.hideTimer = null;
-            $scope.searching = false;
-            $scope.pause = 500;
-            $scope.minLength = 3;
-            $scope.searchStr = null;
-
-            if ($scope.minLengthUser && $scope.minLengthUser != "") {
-                $scope.minLength = $scope.minLengthUser;
-            }
-
-            if ($scope.userPause) {
-                $scope.pause = $scope.userPause;
-            }
-
-            isNewSearchNeeded = function(newTerm, oldTerm) {
-                return newTerm.length >= $scope.minLength && newTerm != oldTerm
-            }
-
-            $scope.processResults = function(responseData, str) {
-                if (responseData && responseData.length > 0) {
-                    $scope.results = [];
-
-                    var titleFields = [];
-                    if ($scope.titleField && $scope.titleField != "") {
-                        titleFields = $scope.titleField.split(",");
-                    }
-
-                    for (var i = 0; i < responseData.length; i++) {
-                        // Get title variables
-                        var titleCode = [];
-
-                        for (var t = 0; t < titleFields.length; t++) {
-                            titleCode.push(responseData[i][titleFields[t]]);
-                        }
-
-                        var description = "";
-                        if ($scope.descriptionField) {
-                            description = responseData[i][$scope.descriptionField];
-                        }
-
-                        var imageUri = "";
-                        if ($scope.imageUri) {
-                            imageUri = $scope.imageUri;
-                        }
-
-                        var image = "";
-                        if ($scope.imageField) {
-                            image = imageUri + responseData[i][$scope.imageField];
-                        }
-
-                        var text = titleCode.join(' ');
-                        if ($scope.matchClass) {
-                            var re = new RegExp(str, 'i');
-                            var strPart = text.match(re)[0];
-                            text = $sce.trustAsHtml(text.replace(re, '<span class="'+ $scope.matchClass +'">'+ strPart +'</span>'));
-                        }
-
-                        var resultRow = {
-                            title: text,
-                            description: description,
-                            image: image,
-                            originalObject: responseData[i]
-                        }
-
-                        $scope.results[$scope.results.length] = resultRow;
-                    }
-
-
-                } else {
-                    $scope.results = [];
-                }
-            }
-
-            $scope.searchTimerComplete = function(str) {
-                // Begin the search
-
-                if (str.length >= $scope.minLength) {
-                    if ($scope.localData) {
-                        var searchFields = $scope.searchFields.split(",");
-
-                        var matches = [];
-
-                        for (var i = 0; i < $scope.localData.length; i++) {
-                            var match = false;
-
-                            for (var s = 0; s < searchFields.length; s++) {
-                                match = match || (typeof $scope.localData[i][searchFields[s]] === 'string' && typeof str === 'string' && $scope.localData[i][searchFields[s]].toLowerCase().indexOf(str.toLowerCase()) >= 0);
-                            }
-
-                            if (match) {
-                                matches[matches.length] = $scope.localData[i];
-                            }
-                        }
-
-                        $scope.searching = false;
-                        $scope.processResults(matches, str);
-
-                    } else {
-                        $http.get($scope.url + str, {}).
-                            success(function(responseData, status, headers, config) {
-                                $scope.searching = false;
-                                $scope.processResults((($scope.dataField) ? responseData[$scope.dataField] : responseData ), str);
-                            }).
-                            error(function(data, status, headers, config) {
-                                console.log("error");
-                            });
-                    }
-                }
-            }
-
-            $scope.hideResults = function() {
-                $scope.hideTimer = $timeout(function() {
-                    $scope.showDropdown = false;
-                }, $scope.pause);
-            };
-
-            $scope.resetHideResults = function() {
-                if($scope.hideTimer) {
-                    $timeout.cancel($scope.hideTimer);
-                };
-            };
-
-            $scope.hoverRow = function(index) {
-                $scope.currentIndex = index;
-            }
-
-            $scope.keyPressed = function(event) {
-                if (!(event.which == 38 || event.which == 40 || event.which == 13)) {
-                    if (!$scope.searchStr || $scope.searchStr == "") {
-                        $scope.showDropdown = false;
-                        $scope.lastSearchTerm = null
-                    } else if (isNewSearchNeeded($scope.searchStr, $scope.lastSearchTerm)) {
-                        $scope.lastSearchTerm = $scope.searchStr
-                        $scope.showDropdown = true;
-                        $scope.currentIndex = -1;
-                        $scope.results = [];
-
-                        if ($scope.searchTimer) {
-                            $timeout.cancel($scope.searchTimer);
-                        }
-
-                        $scope.searching = true;
-
-                        $scope.searchTimer = $timeout(function() {
-                            $scope.searchTimerComplete($scope.searchStr);
-                        }, $scope.pause);
-                    }
-                } else {
-                    event.preventDefault();
-                }
-            }
-
-            $scope.selectResult = function(result) {
-                if ($scope.matchClass) {
-                    result.title = result.title.toString().replace(/(<([^>]+)>)/ig, '');
-                }
-                $scope.searchStr = $scope.lastSearchTerm = result.title;
-                $scope.selectedObject = result;
-                $scope.showDropdown = false;
-                $scope.results = [];
-                //$scope.$apply();
-            }
-
-            var inputField = elem.find('input');
-
-            inputField.on('keyup', $scope.keyPressed);
-
-            elem.on("keyup", function (event) {
-                if(event.which === 40) {
-                    if ($scope.results && ($scope.currentIndex + 1) < $scope.results.length) {
-                        $scope.currentIndex ++;
-                        $scope.$apply();
-                        event.ventDefault;
-                        event.stopPropagation();
-                    }
-
-                    $scope.$apply();
-                } else if(event.which == 38) {
-                    if ($scope.currentIndex >= 1) {
-                        $scope.currentIndex --;
-                        $scope.$apply();
-                        event.preventDefault;
-                        event.stopPropagation();
-                    }
-
-                } else if (event.which == 13) {
-                    if ($scope.results && $scope.currentIndex >= 0 && $scope.currentIndex < $scope.results.length) {
-                        $scope.selectResult($scope.results[$scope.currentIndex]);
-                        $scope.$apply();
-                        event.preventDefault;
-                        event.stopPropagation();
-                    } else {
-                        $scope.results = [];
-                        $scope.$apply();
-                        event.preventDefault;
-                        event.stopPropagation();
-                    }
-
-                } else if (event.which == 27) {
-                    $scope.results = [];
-                    $scope.showDropdown = false;
-                    $scope.$apply();
-                } else if (event.which == 8) {
-                    $scope.selectedObject = null;
-                    $scope.$apply();
-                }
-            });
-
-        }
-    };
-});
-
+             //离开结果区域,移除样式
+             $scope.hoverLeave = function () {
+                 $scope.selectIndex = -1;
+             }
+         }
+     }
+ })
 	      
